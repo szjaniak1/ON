6967a4750da763cb57f905fcc87b5e00b09decc1
szjaniak1 <szjaniak1@gmail.com>
2023-10-17 18:45:34 +0200

    ex 2 and 6 from the first list

---
+++ Lista1/zad2.jl
@@ -0,0 +1,8 @@
+result16::Float16 = 3*(4/3 - 1) - 1
+println(result16)
+
+result32::Float32 = 3*(4/3 - 1) - 1
+println(result32)
+
+result64::Float64 = 3*(4/3 - 1) - 1
+println(result64)
---
+++ Lista1/zad6.jl
@@ -0,0 +1,12 @@
+function f(x)
+	sqrt(x^2 + 1) - 1
+end
+
+function g(x)
+	x^2 / (sqrt(x^2 + 1) + 1)
+end
+
+for i in 1:10
+	println(f(8.0^(-i)))
+	println(g(8.0^(-i)))
+end
