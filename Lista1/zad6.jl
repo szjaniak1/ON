5888a174ab805612e7936a7f40cec0e4c5e06769
szjaniak <szjaniak1@gmail.com>
2023-10-23 01:55:35 +0200

    final

--- Lista1/zad6.jl
+++ Lista1/zad6.jl
@@ -4,15 +4,15 @@ Obliczenia Naukowe
 Lista 1, zadanie 6
 =#
 
-function f(x)
-	sqrt(x^2 + 1) - 1
+function f(x::Float64)::Float64
+	Float64(sqrt(Float64(x^2) + one(Float64)) - one(Float64))
 end
 
-function g(x)
-	x^2 / (sqrt(x^2 + 1) + 1)
+function g(x::Float64)::Float64
+	Float64(Float64(x^2) / (Float64(sqrt(Float64(x^2) + one(Float64))) + one(Float64)))
 end
 
 for i in 1:10
-	println(f(8.0^(-i)))
-	println(g(8.0^(-i)))
+	println(f(Float64(8.0^(-i))))
+	println(g(Float64(8.0^(-i))))
 end
--- Lista1/zad7.jl
+++ Lista1/zad7.jl
@@ -5,11 +5,11 @@ Lista 1, zadanie 7
 =#
 
 function f(x::Float64)::Float64
-	sin(x) + cos(3*x)
+	Float64(sin(x)) + Float64(cos(Float64(3*x)))
 end
 
 function derivative(x::Float64)::Float64
-	cos(x) - 3 * sin(3 * x)
+	Float64(cos(x)) - Float64(3 * Float64(sin(Float64(3 * x))))
 end
 
 function approx(x::Float64, h::Float64)::Float64
@@ -20,6 +20,6 @@ x::Float64 = 1.0
 res::Float64 = 0.0;
 for n in 0:54
 	res = approx(x, Float64(2.0^-n))
-	println(string(res) * " -- " * string(abs(derivative(x) - res)))
+	println(string(n) * " -- " * string(res) * " -- " * string(abs(derivative(x) - res)))
 	println(string(Float64(1.0 + 2.0^-n)))
 end
