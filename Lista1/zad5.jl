f96599f36a6e2e5a114f61eac44ca93ab9ab37d3
szjaniak <szjaniak1@gmail.com>
2023-10-22 19:08:03 +0200

    ex 5 refactor

--- Lista1/zad5.jl
+++ Lista1/zad5.jl
@@ -4,26 +4,26 @@ Obliczenia Naukowe
 Lista 1, zadanie 5
 =#
 
-function forward64(x, y)::Float64
-	S::Float64 = 0.0
+function forward(x, y, type)::type
+	S::type = 0.0
 	for i in 1:5
 		S = S + x[i] * y[i]
 	end
 	S
 end
 
-function backward64(x, y)::Float64
-	S::Float64 = 0.0
+function backward(x, y, type)::type
+	S::type = 0.0
 	for i in 5:-1:1
 		S = S + x[i] * y[i]
 	end
 	S
 end
 
-function c64(x, y)::Float64
-	S::Float64 = 0.0
-	S_negative = Float64[]
-	S_positive = Float64[]
+function c(x, y, type)::type
+	S::type = 0.0
+	S_negative = type[]
+	S_positive = type[]
 
 	for i in 1:5
 		val = x[i] * y[i]
@@ -50,10 +50,10 @@ function c64(x, y)::Float64
 	S
 end
 
-function contrary_c64(x, y)::Float64
-	S::Float64 = 0.0
-	S_negative = Float64[]
-	S_positive = Float64[]
+function contrary_c(x, y, type)::type
+	S::type = 0.0
+	S_negative = type[]
+	S_positive = type[]
 
 	for i in 1:5
 		val = x[i] * y[i]
@@ -79,94 +79,16 @@ function contrary_c64(x, y)::Float64
 	end
 	S
 end
-
-function forward32(x, y)::Float32
-	S::Float32 = 0.0
-	for i in 1:5
-		S = S + x[i] * y[i]
-	end
-	S
-end
-
-function backward32(x, y)::Float32
-	S::Float32 = 0.0
-	for i in 5:-1:1
-		S = S + x[i] * y[i]
-	end
-	S
-end
-
-function c32(x, y)::Float32
-	S::Float32 = 0.0
-	S_negative = Float32[]
-	S_positive = Float32[]
-
-	for i in 1:5
-		val = x[i] * y[i]
-		if val < 0
-			push!(S_negative, val)
-		else
-			push!(S_positive, val)
-		end
-	end
-
-	sort!(S_negative)
-	sort!(S_positive)
-
-	size_negative = size(S_negative, 1)
-	size_positive = size(S_positive, 1)
-
-	for i in size_positive:-1:1
-		S = S + S_positive[i]
-	end
-
-	for i in 1:size_negative
-		S = S + S_negative[i]
-	end
-	S
-end
-
-function contrary_c32(x, y)::Float32
-	S::Float32 = 0.0
-	S_negative = Float32[]
-	S_positive = Float32[]
-
-	for i in 1:5
-		val = x[i] * y[i]
-		if val < 0
-			push!(S_negative, val)
-		else
-			push!(S_positive, val)
-		end
-	end
-
-	sort!(S_negative)
-	sort!(S_positive)
-
-	size_negative = size(S_negative, 1)
-	size_positive = size(S_positive, 1)
-
-	for i in 1:size_positive
-		S = S + S_positive[i]
-	end
-
-	for i in size_negative:-1:1
-		S = S + S_negative[i]
-	end
-	S
-end
-
-
 
 x = [2.718281828, -3.141592654, 1.414213562, 0.5772156649, 0.3010299957]
 y = [1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
 
-println(forward64(x, y))
-println(backward64(x, y))
-println(c64(x, y))
-println(contrary_c64(x, y))
+println(forward(x, y, Float64))
+println(backward(x, y, Float64))
+println(c(x, y, Float64))
+println(contrary_c(x, y, Float64))
 
-println(forward32(x, y))
-println(backward32(x, y))
-println(c32(x, y))
-println(contrary_c32(x, y))
+println(forward(x, y, Float32))
+println(backward(x, y, Float32))
+println(c(x, y, Float32))
+println(contrary_c(x, y, Float32))
