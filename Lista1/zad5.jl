252801e4f4ba03d06c4cab3c0ed26129ee3f155d
szjaniak <szjaniak1@gmail.com>
2023-10-22 19:18:34 +0200

    ex 5 now getting good results

--- Lista1/zad5.jl
+++ Lista1/zad5.jl
@@ -26,7 +26,7 @@ function c(x, y, type)::type
 	S_positive = type[]
 
 	for i in 1:5
-		val = x[i] * y[i]
+		val::type = x[i] * y[i]
 		if val < 0
 			push!(S_negative, val)
 		else
@@ -40,14 +40,16 @@ function c(x, y, type)::type
 	size_negative = size(S_negative, 1)
 	size_positive = size(S_positive, 1)
 
+	result_positive::type = 0.0
 	for i in size_positive:-1:1
-		S = S + S_positive[i]
+		result_positive = result_positive + S_positive[i]
 	end
 
+	result_negative::type = 0.0
 	for i in 1:size_negative
-		S = S + S_negative[i]
+		result_negative = result_negative + S_negative[i]
 	end
-	S
+	S = result_positive + result_negative
 end
 
 function contrary_c(x, y, type)::type
@@ -56,7 +58,7 @@ function contrary_c(x, y, type)::type
 	S_positive = type[]
 
 	for i in 1:5
-		val = x[i] * y[i]
+		val::type = x[i] * y[i]
 		if val < 0
 			push!(S_negative, val)
 		else
@@ -70,25 +72,27 @@ function contrary_c(x, y, type)::type
 	size_negative = size(S_negative, 1)
 	size_positive = size(S_positive, 1)
 
+	result_positive::type = 0.0
 	for i in 1:size_positive
-		S = S + S_positive[i]
+		result_positive = result_positive + S_positive[i]
 	end
 
+	result_negative::type = 0.0
 	for i in size_negative:-1:1
-		S = S + S_negative[i]
+		result_negative = result_negative + S_negative[i]
 	end
-	S
+	S = result_positive + result_negative
 end
 
 x = [2.718281828, -3.141592654, 1.414213562, 0.5772156649, 0.3010299957]
 y = [1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
 
-println(forward(x, y, Float64))
-println(backward(x, y, Float64))
-println(c(x, y, Float64))
-println(contrary_c(x, y, Float64))
+println(forward(Array{Float64}(x), Array{Float64}(y), Float64))
+println(backward(Array{Float64}(x), Array{Float64}(y), Float64))
+println(c(Array{Float64}(x), Array{Float64}(y), Float64))
+println(contrary_c(Array{Float64}(x), Array{Float64}(y), Float64))
 
-println(forward(x, y, Float32))
-println(backward(x, y, Float32))
-println(c(x, y, Float32))
-println(contrary_c(x, y, Float32))
+println(forward(Array{Float32}(x), Array{Float32}(y), Float32))
+println(backward(Array{Float32}(x), Array{Float32}(y), Float32))
+println(c(Array{Float32}(x), Array{Float32}(y), Float32))
+println(contrary_c(Array{Float32}(x), Array{Float32}(y), Float32))
