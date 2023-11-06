acf537720428388d040b362f8d75f790cfd34406
szjaniak <szjaniak1@gmail.com>
2023-11-06 21:41:42 +0100

    ...

---
+++ Lista3/zad2.jl
@@ -0,0 +1,9 @@
+#=
+Szymon Janiak 268514
+Obliczenia Naukowe
+Lista 3, zadanie 2
+=#
+
+function mstycznych(f::Function, pf::Function, x0::Float64, delta::Float64, epsilon::Float64, maxit::UInt64)
+
+end
