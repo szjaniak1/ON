149e551de4d4718123d2ab1e570cacc80ac8bad1
szjaniak <szjaniak1@gmail.com>
2023-11-16 17:27:13 +0100

    ex2 started

--- Lista3/zad2.jl
+++ Lista3/zad2.jl
@@ -5,5 +5,38 @@ Lista 3, zadanie 2
 =#
 
 function mstycznych(f::Function, pf::Function, x0::Float64, delta::Float64, epsilon::Float64, maxit::UInt64)
+	err::UInt8 = 0
+	it::UInt64 = 0
+	x1 = Float64 = 0.0
+	val::Float64 = f(x0)
+	val_prime::Float64 = pf(x0)
 
+	if abs(val) < epsilon
+		return 0, x0, val, err
+	end
+
+	for it in range(maxit)
+		
+		if abs(val_prime) < epsilon
+			break
+		end
+
+		x1 = x0 - val / val_prime
+
+		if abs(x1 - x0) <= delta
+			return x1
+		end
+
+		x0 = x1
+	end
+
+	return
+end
+
+function func(x::Float64)
+	return x^2 - 2
+end
+
+function func_prime(x::Float64)
+	return 2 * x
 end
