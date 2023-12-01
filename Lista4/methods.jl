91cb7f63e78814574cd771bb39de075439baa5c0
szjaniak1 <szjaniak1@gmail.com>
2023-12-01 15:31:52 +0100

    l4 start

---
+++ Lista4/methods.jl
@@ -0,0 +1,21 @@
+#=
+Szymon Janiak 268514
+Obliczenia Naukowe
+Lista 4, zadanie 1, 2, 3, 4
+=#
+
+function difference_quotients(x::Vector{Float64}, f::Vector{Float64})
+
+end
+
+function newton_value(x::Vector{Float64}, fx::Vector{Float64}, t::Float64)
+
+end
+
+function natural(x::Vector{Float64}, fx::Vector{Float64})
+
+end
+
+function draw_Nnfx(f::Function, a::Float64, b::Float64, n::UInt8)
+
+end
