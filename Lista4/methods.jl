d42f72bc92e53fac83e0a65a9cb28b9a4b9fb563
szjaniak1 <szjaniak1@gmail.com>
2023-12-01 17:51:38 +0100

    difference quotients method done

--- Lista4/methods.jl
+++ Lista4/methods.jl
@@ -4,11 +4,31 @@ Obliczenia Naukowe
 Lista 4, zadanie 1, 2, 3, 4
 =#
 
-function difference_quotients(x::Vector{Float64}, f::Vector{Float64})
+# using Plots
 
+module methods
+
+export difference_quotients
+
+function difference_quotients(x::Vector{Float64}, f::Vector{Float64})::Vector{Float64}
+	fx::Vector{Float64} = []
+	f_copy::Vector{Float64} = copy(f)
+	len::UInt16 = length(x)
+
+	for i in 1:(len)
+		for k in (i - 1):-1:1
+			a = f_copy[k + 1] - f_copy[k]
+			b = (x[i] - x[k])
+            f_copy[k] = a/b
+        end
+        push!(fx, f_copy[1])
+    end
+
+	return fx
 end
 
 function newton_value(x::Vector{Float64}, fx::Vector{Float64}, t::Float64)
+	n = length(x)
 
 end
 
@@ -19,3 +39,5 @@ end
 function draw_Nnfx(f::Function, a::Float64, b::Float64, n::UInt8)
 
 end
+
+end 
---
+++ Lista4/zad1.jl
@@ -0,0 +1,16 @@
+#=
+Szymon Janiak 268514
+Obliczenia Naukowe
+Lista 4, zadanie 1
+=#
+
+
+include("./methods.jl")
+using .methods
+
+x = Vector{Float64}([3, 1, 5, 6])
+f = Vector{Float64}([1, -3, 2, 4])
+
+fx = difference_quotients(x, f)
+
+println(fx)
