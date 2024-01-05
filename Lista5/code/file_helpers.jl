ba7bf5a6a64bfd04585cc7fd8262c0af4286da0f
szjaniak <szjaniak1@gmail.com>
2024-01-05 19:35:10 +0100

    write/read file functions moved to a separate file, created them as module

---
+++ Lista5/code/file_helpers.jl
@@ -0,0 +1,50 @@
+include("./matrixgen.jl")
+using .matrixgen
+
+module file_helpers
+
+export read_A_file, read_B_file, write_X_file
+
+using SparseArrays
+using DelimitedFiles
+
+function read_A_file(file_path::String)
+	file = open(file_path, "r")
+
+	size, block_size = parse.(Int64, split(readline(file), ' '))
+	rows = []
+	columns = []
+	values = []
+
+	while !eof(file)
+		i, j, val = split(readline(file), ' ')
+		push!(rows, parse(Int64, i))
+		push!(columns, parse(Int64, j))
+		push!(values, parse(Float64, val))
+  	end
+
+	close(file)
+	matrix = sparse(rows, columns, values)
+	return matrix, size, block_size
+end
+
+function read_B_file(file_path::String)
+	file = open(file_path, "r")
+
+	result::Vector{Float64} = []
+	size = parse(Int64, readline(file))
+
+	while !eof(file)
+		val = parse(Float64, readline(file))
+		push!(result, val)
+	end
+
+	close(file)
+	return result, size
+end
+
+function write_X_file(file_path::String, results::Vector{Float64})
+	writedlm(file_path, results)
+end
+
+end
--- Lista5/code/main.jl
+++ Lista5/code/main.jl
@@ -1,47 +1,5 @@
-include("./matrixgen.jl")
-using .matrixgen
-
-using SparseArrays
-using DelimitedFiles
-
-function read_A_file(file_path::String)
-	file = open(file_path, "r")
-
-	size, block_size = parse.(Int64, split(readline(file), ' '))
-	rows = []
-	columns = []
-	values = []
-
-	while !eof(file)
-		i, j, val = split(readline(file), ' ')
-		push!(rows, parse(Int64, i))
-		push!(columns, parse(Int64, j))
-		push!(values, parse(Float64, val))
-  	end
-
-	close(file)
-	matrix = sparse(rows, columns, values)
-	return matrix, size, block_size
-end
-
-function read_B_file(file_path::String)
-	file = open(file_path, "r")
-
-	result::Vector{Float64} = []
-	size = parse(Int64, readline(file))
-
-	while !eof(file)
-		val = parse(Float64, readline(file))
-		push!(result, val)
-	end
-
-	close(file)
-	return result, size
-end
-
-function write_X_file(file_path::String, results::Vector{Float64})
-	writedlm(file_path, results)
-end
+include("./file_helpers.jl")
+using .file_helpers
 
 res_b, siz_b = read_B_file("../data/Dane16_1_1/b.txt")
 # res, siz, blck_siz = read_A_file("./A.txt")
